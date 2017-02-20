8675f97e6761bee28fbf7b8e302f91569ea4d386
Sergio Lindo Mansilla <slindomansilla@suse.com>
2017-02-20 17:38:39 +0100

    Improve git-sync-fork.sh
    - Now you can send a list of branch names to be sync as parameters.
    - Improve documentation.

--- root/usr/local/bin/git-sync-fork.sh
+++ root/usr/local/bin/git-sync-fork.sh
@@ -2,14 +2,26 @@
 
 # Name: git-sync-fork.sh
 #
-# It updates the branches in fork with the branches in the upstream repository.
+# Synopsis:
+#   git-sync-fork.sh [BRANCH]...
+#   Example: git-sync-fork.sh master gh-pages
 #
-# Assumptions:
-# - The current directory is the git project.
-# - The name of the remote for the upstream repository is 'origin'.
-# - The name of the remote for the fork repository is 'fork'.
+# Description:
+#   It updates the branches in fork with the branches in the upstream repository.
+#
+#   Assumptions:
+#   - The current directory is the git project.
+#   - The name of the remote for the upstream repository is 'origin'.
+#   - The name of the remote for the fork repository is 'fork'.
+#
 
-branches_to_sync='master' && \
+
+if [ "$#" -gt 0 ]; then
+  branches_to_sync=$@
+else
+  branches_to_sync="master"
+fi
+
 git remote update --prune && \
 for branch_name in $branches_to_sync; do
   git checkout $branch_name && \
