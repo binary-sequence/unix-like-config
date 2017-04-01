b3c594c59b5d42cc2ff403b61ad5f9b4f62b0ba0
Sergio Lindo <sergiolindo.empresa@gmail.com>
2017-04-01 14:57:45 +0200

    Show battery status a little bit more verbose mode

--- root/usr/local/bin/showBatteryStatus.sh
+++ root/usr/local/bin/showBatteryStatus.sh
@@ -7,8 +7,8 @@ if [ ! -f /sys/class/power_supply/BAT1/uevent ]; then
   exit 1
 fi
 
-state=`upower -i $batteryFile | grep "state" | sed 's/ //g; s/charging/c/; s/discharging/d/' | cut -d: -f2`
+state=`upower -i $batteryFile | grep "state" | sed 's/ //g' |  cut -d: -f2`
 percentage=`upower -i $batteryFile | grep "percentage" | sed 's/ //g' | cut -d: -f2`
 timeTo=`upower -i $batteryFile | grep "time to" | sed 's/ //g; s/,[0-9]*//; s/minutes/min/' | cut -d: -f2`
 
-echo "$state:$timeTo:$percentage"
+echo "($percentage) $state: $timeTo"
